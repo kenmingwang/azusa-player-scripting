d68699f84e6126bc1d4522bbfea485f9547d17d6
kenmingwang <fengfeng8867dd@hotmail.com>
2026-04-26 19:10:19 +0800

    Set up remote AVPlayer repro entry

--- index.tsx
+++ index.tsx
@@ -1,13 +1,1 @@
-import { Navigation, Script } from "scripting";
-
-import { DefaultPlaylistApp } from "./lib/defaultPlaylistApp";
-
-async function run() {
-  await Navigation.present({
-    element: <DefaultPlaylistApp />,
-  });
-
-  Script.exit();
-}
-
-void run();
+import "./avplayer_repro";
